81f975bd28ba33acdfd4bd6e503d071c7f0e3ae3
Stefan Wurzinger <swurzinger@gmx.at>
2025-02-01 13:08:53 +0100

    minor fixes
    (cherry picked from commit 8575a554a4f041275f783357599da3472b458885)

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -982,6 +982,7 @@ class TransactionService
         $transactionHistory->status = TransactionHistory::STATUS_ACTIVE;
         $transactionHistory->order_id = $order->id;
         $transactionHistory->rule_id = $rule->id;
+        $transactionHistory->transaction_id = null;
         $transactionHistory->save();
 
         return $transactionHistory;
