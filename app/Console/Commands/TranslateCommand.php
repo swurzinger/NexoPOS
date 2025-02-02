deb83ccb821a430f742da20a5e2e07bd2499754c
Stefan Wurzinger <swurzinger@gmx.at>
2025-02-02 01:44:52 +0100

    sort translation strings according to base translation (en)

--- app/Console/Commands/TranslateCommand.php
+++ app/Console/Commands/TranslateCommand.php
@@ -17,7 +17,7 @@ class TranslateCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'ns:translate {module?} {--extract} {--lang=en} {--build}';
+    protected $signature = 'ns:translate {module?} {--extract} {--lang=en} {--build} {--sort}';
 
     /**
      * The console command description.
@@ -50,6 +50,8 @@ class TranslateCommand extends Command
             $this->extracting();
         } elseif ( $this->option( 'build' ) ) {
             $this->build();
+        } elseif ( $this->option( 'sort' ) ) {
+            $this->sort();
         }
     }
 
@@ -276,5 +278,58 @@ class TranslateCommand extends Command
                 return ["$namespace.$key" => $value];
             } )->toArray();
         }
+    }
+
+    private function sort()
+    {
+        $defaultLocale = 'en';
+        $locale = $this->option( 'lang' );
+
+        if ( $this->argument( 'module' ) ) {
+            $module = $this->modulesService->get( $this->argument( 'module' ) );
+
+            if ( ! empty( $module ) ) {
+                $basePath = Str::finish( $module[ 'lang-relativePath' ], DIRECTORY_SEPARATOR );
+            } else {
+                $this->error( __( 'Unable to find the requested module.' ) );
+                return;
+            }
+        } else {
+            $basePath = 'lang/';
+        }
+
+        $defaultFilePath = $basePath . $defaultLocale . '.json';
+        $filePath = $basePath . $locale . '.json';
+
+        if ( ! Storage::disk( 'ns' )->exists( $defaultFilePath ) ) {
+            $this->error( 'Unable to find: ' . $defaultFilePath );
+            return;
+        }
+        if ( ! Storage::disk( 'ns' )->exists( $filePath ) ) {
+            $this->error( 'Unable to find: ' . $filePath );
+            return;
+        }
+
+        $defaultKeys = array_keys( json_decode( Storage::disk( 'ns' )->get( $defaultFilePath ), true ) );
+        $lang = json_decode( Storage::disk( 'ns' )->get( $filePath ), true );
+
+        uksort( $lang, function ( $a, $b ) use ( $defaultKeys ) {
+            $ia = array_search( $a, $defaultKeys );
+            $ib = array_search( $b, $defaultKeys );
+
+            if ( $ia === $ib ) {
+                return 0;
+            }
+            if ( $ia === false ) {
+                return 1;
+            }
+            if ( $ib === false ) {
+                return -1;
+            }
+
+            return $ia - $ib;
+        } );
+
+        Storage::disk( 'ns' )->put( $filePath, json_encode( $lang, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES ) );
     }
 }
